c59489cafdec8e19f0a2d87cdced7a75ec9b6199
Matus Tejiscak <ziman@functor.sk>
2011-09-26 01:02:48 +0200

    Comments written.

--- ecomp.v
+++ ecomp.v
@@ -1,8 +1,7 @@
 Require Import
   Utf8.
 
-(* Code *)
-
+(* Operators and expressions.  *)
 Inductive op : Set :=
   | add : op
   | mul : op.
@@ -17,6 +16,7 @@ Notation "[ x ]" := (num x).
 
 Example sample_expr := [3] .* [4] .+ [5] .* [9].
 
+(* Denotational semantics of ops and expressions. *)
 Definition d_op (o : op) : nat → nat → nat :=
   match o with
   | add => plus
@@ -31,14 +31,18 @@ Fixpoint denotation (e : expr) : nat :=
 
 Eval compute in (denotation sample_expr).
 
+(* Instruction of a stack machine of type [instr s t] transforms
+    a stack of size [s] into a stack of size [t]. *)
 Inductive instr : nat → nat → Set :=
   | ipush : forall {s}, nat → instr s (S s)
   | ibiop : forall {s}, op → instr (S (S s)) (S s).
 
+(* Code is a snoc-sequence of instructions, indexed by its action on stack. *)
 Inductive code : nat → nat → Set :=
   | cnil : forall {s}, code s s
   | csnoc : forall {s t u}, code s t → instr t u → code s u.
 
+(* Concatenation of codes. *)
 Fixpoint cappend {s t u : nat} (p : code s t) (q : code t u) : code s u.
   destruct q;
     assumption ||
@@ -46,6 +50,7 @@ Fixpoint cappend {s t u : nat} (p : code s t) (q : code t u) : code s u.
 Defined.
 Print cappend.
 
+(* Compilation of an expression into code. *)
 Fixpoint compile (e : expr) {s : nat} : code s (S s) :=
   match e with
   | num x => csnoc cnil (ipush x)
@@ -54,6 +59,7 @@ Fixpoint compile (e : expr) {s : nat} : code s (S s) :=
 
 Eval compute in @compile sample_expr 0.
 
+(* A size-indexed stack. *)
 Inductive stack (A : Set) : nat → Set :=
   | snil : stack A O
   | spush : forall {n}, A → stack A n → stack A (S n).
@@ -63,11 +69,13 @@ Definition pop {A} {s} (st : stack A (S s)) :=
   | spush _ x xs => (x,xs)
   end.
 
+(* Operational semantics of a binary operator. *)
 Definition exec_op (o : op) {s} (st : stack nat (S (S s))) : stack nat (S s) :=
   let (y,st') := pop st in
   let (x,st'') := pop st' in
   spush _ (d_op o x y) st''.
-  
+ 
+(* Operational semantics of an instruction. *)
 Definition exec {s t} (i : instr s t) : stack nat s → stack nat t.
   destruct i;
     exact (spush _ n) ||
@@ -75,6 +83,7 @@ Definition exec {s t} (i : instr s t) : stack nat s → stack nat t.
 Defined.
 Print exec.    
 
+(* Operational semantics of a code sequence. *)
 Fixpoint run {s t} (c : code s t) (st : stack nat s) : stack nat t.
   destruct c;
     assumption ||
@@ -82,12 +91,13 @@ Fixpoint run {s t} (c : code s t) (st : stack nat s) : stack nat t.
 Defined.
 Print run.
 
+(* Some utilities. *)
 Definition empty_stack {A} := snil A.
 Definition singleton {A : Set} (x : A) := spush A x empty_stack.
 
 Eval compute in run (compile sample_expr) empty_stack.
 
-(* Proofs *)
+(* === Proofs === *)
 
 Lemma cappend_cnil : forall {s t} (p : code s t), cappend cnil p = p.
   intros s t; induction p;
@@ -103,6 +113,7 @@ Proof.
     simpl; rewrite (IHq p st); reflexivity.
 Qed.
 
+(* A variation of the correctness theorem, operating on any stack. *)
 Lemma correctness_strong : forall (e : expr) {s} {st : stack nat s}, run (compile e) st = spush _ (denotation e) st. 
 Proof.
   induction e; try reflexivity.
@@ -113,6 +124,7 @@ Proof.
   unfold exec_op; reflexivity.
 Qed.
 
+(* The main theorem proving the correctness of the compiler and interpreter. *)
 Theorem correctness : forall e : expr, run (compile e) empty_stack = singleton (denotation e).
 Proof.
   intro e; apply (correctness_strong e).
